d3dab9c42bf9dd824b6276a652dc122e526c1cf8
mdong151 <mdong151@gmail.com>
2019-12-29 09:14:09 +0700

    Learn: common asertion

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -23,13 +23,28 @@ test.skip('Test case No. 1', async t => {
     await t.hover('selector', { options })
     await t.selectText('selector', { options })
     await t.typeText('selector', 'textToType', { options })
-    //key: backspace, enter
+    // key: backspace, enter
     await t.pressKey('backspace', { options })
     await t.navigateTo('url')
     await t.takeScreenshot()
     await t.takeElementScreenshot()
     await t.resizeWindow(800, 600)
-    
+
+    // Deep equal
+    await t.expect('foo').eql('foo', 'message when failed', { options })
+    await t.expect('foo').notEql('foo', 'message when failed', { options })
+    // OK
+    await t.expect('foo').ok()
+    // Not OK
+    await t.expect('foo').notOk()
+    // Contains
+    await t.expect('expected string').contains('expected')  // -> true
+    // Greater or Less than
+    await t.expect(10).gt(5)    // -> true
+    await t.expect(10).gte(10)  // -> true
+
+    // Within or not Within
+    await t.expect(10).within(1, 100)    // -> true
 
 
 
