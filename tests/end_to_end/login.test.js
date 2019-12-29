908ee9bd0d871a4353ff23749f58c3410ea73163
mdong151 <mdong151@gmail.com>
2019-12-29 09:14:28 +0700

    Added end to end test

---
+++ tests/end_to_end/login.test.js
@@ -0,0 +1,10 @@
+import { Selector, t } from 'testcafe'
+
+// pretier-ignore
+fixture`End to End test suites`
+    .page`http://zero.webappsecurity.com/`
+
+
+test('User cannot Login with invalid credentials', async t => {
+
+})
